6a126697780ff4eb239d4d04a21d362b652ff795
Roodmann <dima_rood@inbox.ru>
2026-01-13 19:57:01 +0300

    complete set4 added classes

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    context = {
-        'app_name': 'hexlet_django_blog.article'
-    }
-    return render(request, 'articles/index.html', context)
+class IndexView(View):
+    template_name = 'articles/index.html'
+
+    def get(self, request):
+        context = {
+            'app_name': 'hexlet_django_blog.article'
+        }
+        return render(request, self.template_name, context)
 # Create your views here.
--- hexlet_django_blog/hexlet_django_blog/views.py
+++ hexlet_django_blog/hexlet_django_blog/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
+class IndexView(TemplateView):
+    template_name = "index.html"
 
-def index(request):
-    return render(
-        request,
-        "index.html",
-        context={
-            "who": "World",
-        },
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["who"] = "World"
+        return context
 
 
 def about(request):
