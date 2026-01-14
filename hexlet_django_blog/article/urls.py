10f49ef1d25d3703bd7b1f76d99bb9dcbe7cd1a9
Roodmann <dima_rood@inbox.ru>
2026-01-14 20:27:16 +0300

    complete set5 added added routes and a redirect

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.home_redirect, name='home'),
+    path("articles/<str:tags>/<int:article_id>/", views.index, name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 class IndexView(View):
     template_name = 'articles/index.html'
 
-    def get(self, request):
+    def get(self, request, tags=None, article_id=None):
+        message = f"Статья номер {article_id}. Тег {tags}"
         context = {
+            'message': message,
             'app_name': 'hexlet_django_blog.article'
         }
         return render(request, self.template_name, context)
+
+
+def home_redirect(request):
+    url = reverse('article', kwargs={'tags': 'python', 'article_id': 42})
+    return redirect(url)
 # Create your views here.
