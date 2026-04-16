3cfd4cb9dacae3ed39ad824ce8e60f43ea82c5be
Roodmann <dima_rood@inbox.ru>
2026-04-16 22:41:37 +0300

    Добавил форму регистрации пользователя

--- hexlet_django_blog/hexlet_django_blog/urls.py
+++ hexlet_django_blog/hexlet_django_blog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hexlet_django_blog.views import IndexView
+from hexlet_django_blog.views import IndexView, RegisterView
 from hexlet_django_blog import views
 
 urlpatterns = [
@@ -24,5 +24,6 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("articles/", include("hexlet_django_blog.article.urls")),
     path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register'),
 
 ]
--- hexlet_django_blog/hexlet_django_blog/views.py
+++ hexlet_django_blog/hexlet_django_blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 from hexlet_django_blog.article.models import Article
+from .forms import RegistrationForm
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -10,6 +13,15 @@ class IndexView(TemplateView):
         context["who"] = "World"
         context["articles"] = Article.objects.all()
         return context
+
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegistrationForm
+
+    def form_valid(self, form):
+        form.save()
+        return redirect('index')
 
 
 def about(request):
